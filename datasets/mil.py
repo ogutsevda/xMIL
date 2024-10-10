a68ffe781294c97480f6b11865880ace2e9dbcd1
Mina Jamshidi <mina@Minas-MacBook-Air-4.local>
2024-10-10 11:18:05 +0200

    MIL dataset updated to have sample_id

--- datasets/mil.py
+++ datasets/mil.py
@@ -7,14 +7,15 @@ from tqdm import tqdm
 from datasets.data_handler import MetadataHandler, SlideDataHandler
 
 
-class MILFeatureDataset(Dataset):
+class MILSlideDataset(Dataset):
 
     def __init__(self, split_path, metadata_dirs, subsets, patches_dirs, features_dirs, label_cols,
-                 bag_size=None, patch_filters=None, preload_features=False, drop_duplicates='sample',
-                 max_bag_size=None):
-        super(MILFeatureDataset, self).__init__()
+                 bag_size=None, num_repetitions=1, patch_filters=None, preload_features=False,
+                 drop_duplicates='sample', max_bag_size=None, min_bag_size=0):
+        super(MILSlideDataset, self).__init__()
         # Save args
         self.bag_size = bag_size
+        self.num_repetitions = num_repetitions
         self.features_dirs = features_dirs
         self.label_cols = label_cols
         self.num_targets = len(self.label_cols)
@@ -39,10 +40,12 @@ class MILFeatureDataset(Dataset):
         self.feature_indices, self.patch_ids = \
             SlideDataHandler.load_patch_metadata(self.split_metadata, patches_dirs, patch_filters)
         # Drop bags with too many patches
-        if max_bag_size is not None and max_bag_size > 0:
-            keep_slides = [idx for idx in self.split_metadata.index if len(self.patch_ids[idx]) <= max_bag_size]
+        if max_bag_size is not None and max_bag_size > 0 or min_bag_size > 0:
+            max_bag_size = torch.inf if (max_bag_size is None or max_bag_size < 0) else max_bag_size
+            keep_slides = [idx for idx in self.split_metadata.index if
+                           min_bag_size <= len(self.patch_ids[idx]) <= max_bag_size]
             print(f"Dropping {len(self.split_metadata) - len(keep_slides)} slides with more than {max_bag_size} "
-                  f"patches.")
+                  f"or fewer than {min_bag_size} patches.")
             self.split_metadata = self.split_metadata.loc[keep_slides].reset_index(drop=True)
             self.feature_indices = [self.feature_indices[idx] for idx in keep_slides]
             self.patch_ids = [self.patch_ids[idx] for idx in keep_slides]
@@ -67,10 +70,10 @@ class MILFeatureDataset(Dataset):
         for key in batch_list[0].keys():
             if key in ['features', 'patch_ids']:
                 col_batch[key] = torch.concat([batch[key] for batch in batch_list])
-            elif key == 'slide_id':
-                col_batch[key] = [batch[key] for batch in batch_list]
             elif key == 'targets':
                 col_batch[key] = torch.stack([batch[key] for batch in batch_list])
+            elif key == 'sample_ids':
+                col_batch[key] = {col: [batch[key][col] for batch in batch_list] for col in batch_list[0][key]}
             else:
                 col_batch[key] = torch.tensor([batch[key] for batch in batch_list])
         return col_batch
@@ -79,7 +82,7 @@ class MILFeatureDataset(Dataset):
         return self.split_metadata
 
     def __len__(self):
-        return len(self.split_metadata)
+        return len(self.split_metadata) * self.num_repetitions
 
     def __getitem__(self, idx):
         """
@@ -92,6 +95,7 @@ class MILFeatureDataset(Dataset):
             - 'patch_ids': (torch.Tensor) Identifiers of the patches associated to the features; patch_ids[idx]
                 corresponds to feature[idx].
         """
+        idx = idx // self.num_repetitions
         # Load relevant metadata
         source_id, slide_id = self.split_metadata.loc[idx, ['source_id', 'slide_id']]
         patch_ids = self.patch_ids[idx]
@@ -105,4 +109,4 @@ class MILFeatureDataset(Dataset):
         if self.bag_size is not None and self.bag_size > 0:
             features, patch_ids = SlideDataHandler.sample_features(features, self.bag_size, patch_ids)
         return {'features': features, 'bag_size': len(features), 'targets': targets, 'patch_ids': patch_ids,
-                'source_id': source_id, 'slide_id': slide_id}
+                'sample_ids': {'source_id': source_id, 'slide_id': slide_id}}
